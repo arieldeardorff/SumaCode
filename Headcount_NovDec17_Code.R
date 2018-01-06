494f38c74c102ff8f499a742f4bfcdde203d1d0a
Ariel Deardorff <arieldeardorff@users.noreply.github.com>
2018-01-05 16:57:53 -0800

    Added R code

---
+++ Headcount_NovDec17_Code.R
@@ -0,0 +1,27 @@
+Ariel Deardorff, UCSF Library
+January 5, 2017
+
+#The goal of this code is to tranform raw SUMA data into a format that is more easily readable in Tableau.
+
+# in excel get rid of empty rows and header rows
+
+data <- read.csv("Headcount_NovDec17_Raw.csv", header = TRUE, na.strings = c(" ","NA"))
+
+#load packages
+library(reshape2)
+library(dplyr)
+library(tidyr)
+
+#melt data from the first 6 columns
+data_melt <- melt(data, id=c(1:6), na.rm = TRUE, value.name = "Count")
+
+# change empty cells to NA
+data_melt$Count[data_melt$Count==""] <- NA
+
+#omit NA cells 
+data_melt <- na.omit(data_melt)
+
+#separate variable column into two new column on the tenth character
+data_melt <- separate(data_melt, variable, c("Activity","Seating"), sep= 10, remove = FALSE, fill = "right")
+
+write.csv(data_melt, file="Headcount_NovDec17_Final.csv")
